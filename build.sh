e586b73e18dc3bdd1626b23a9757b1b112d3c765
Mohamad Fakih <mfakih294@gmail.com>
2020-06-23 12:20:32 +0300

    v1.2.7: Setting the IP is now set using an input on home screen instead of a modal pop up.v1.2.7: Setting the IP is now set using an input on home screen instead of a modal pop up..

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-export PATH=/home/alef/Android/Sdk/tools:/home/alef/Android/Sdk/tools/bin:/home/alef/Android/Sdk/build-tools/29.0.2:/home/alef/Android/Sdk/platform-tools:/home/alef/Documents/dev/jdk1.8.0_65/bin:/home/alef/Documents/dev/jdk1.8.0_65/jre/bin:$PATH
+export PATH=/nbr/dev/Android/Sdk/tools:/nbr/dev/Android/Sdk/tools/bin:/nbr/dev/Android/Sdk/build-tools/29.0.2:/nbr/dev/Android/Sdk/platform-tools:/usr/lib/jvm/java-8-openjdk-amd64/bin:/usr/lib/jvm/java-8-openjdk-amd64/bin/:$PATH
 
-export JAVA_HOME=/home/alef/Documents/dev/jdk1.8.0_65/
-export ANDROID_HOME=/home/alef/Android/Sdk
-export ANDROID_SDK_ROOT=/home/alef/Android/Sdk
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
+export ANDROID_HOME=/nbr/dev/Android/Sdk
+export ANDROID_SDK_ROOT=/nbr/dev/Android/Sdk
 
 #ionic serve
 #ionic cordova run android  -l -c --device
@@ -10,12 +10,13 @@ export ANDROID_SDK_ROOT=/home/alef/Android/Sdk
 ionic cordova build android --prod --release
 #cp 	/home/alef/Documents/dev/nbrm/platforms/android/app/build/outputs/apk/debug/app-debug.apk 	/nbr/rps1/nibras.apk
 
-rm /home/alef/Documents/dev/nbrm/platforms/android/app/build/outputs/apk/release/NibrasReader.apk
+#rm /home/alef/Documents/dev/nbrm/platforms/android/app/build/outputs/apk/release/NibrasReader.apk
 
-cd /home/alef/Documents/dev/nbrm/platforms/android/app/build/outputs/apk/release
+cd /nbr/dev/mbl/platforms/android/app/build/outputs/apk/release
+
 
 jarsigner -verbose -sigalg SHA1withRSA -storepass QAZWSX -digestalg SHA1 -keystore my-release-key.keystore app-release-unsigned.apk nibras_reader
 
 zipalign -v 4 app-release-unsigned.apk NibrasReader.apk
-cp NibrasReader.apk ~/dev/dist/NibrasReader.apk
+cp NibrasReader.apk /nbr/dev/mbl/NibrasReader-final.apk
 
